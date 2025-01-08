d8864852826a39efe4cf06bf14240de5f61b3231
jianwong <jianwong@student.42kl.edu.my>
2025-01-08 19:12:30 +0800

    prototype for bonus

---
+++ philo_bonus/includes/philo_bonus.h
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jianwong <jianwong@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 18:19:57 by jianwong          #+#    #+#             */
+/*   Updated: 2025/01/08 18:20:25 by jianwong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+// maximum number of philos
+# define MAX_PHILOS 200
+
+// int array index
+# define NUM_PHILOS 0
+# define TIME_DIE 1
+# define TIME_EAT 2
+# define TIME_SLEEP 3
+# define MIN_MEALS 4
+# define NOT_SET -1
+
+// each philo metadata
+typedef struct s_philo
+{
+	int					index;
+	int					total_philo;
+	size_t				time_sleep;
+	size_t				time_eat;
+	size_t				time_die;
+	int					meals_ate;
+	int					min_meals;	
+	size_t				start_time;
+	size_t				last_ate;
+	int					is_dead;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
+	pthread_mutex_t		*is_alive_mutex;
+	pthread_mutex_t		*is_philo_ready_mutex;
+}					t_philo;
+
+// philo_utils
+int			ft_atoi(const char *nptr);
+size_t		get_current_time(void);
+
+// checker
+int			check_input(int argc, char **argv, int *vars);
+
+// metadata
+t_philo		*create_philo_metadatas(int *vars, pthread_mutex_t **forks);
+void		free_metadata(t_philo *metadatas, pthread_mutex_t *forks);
+
+// routine
+void		*philo_routine(void *args);
+
+// routine utils
+int			is_philo_dead(t_philo *metadata);
+void		philo_grab_fork(pthread_mutex_t *fork, \
+t_philo *metadata, size_t start);
+
+// monitoring
+void		*monitor_routine(void *args);
+
+#endif // !PHILO_H
---
+++ philo_bonus/src/philo_bonus_utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jianwong <jianwong@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 18:29:03 by jianwong          #+#    #+#             */
+/*   Updated: 2025/01/08 18:29:17 by jianwong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo_bonus.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	result;
+	int	is_nega;
+
+	is_nega = 0;
+	result = 0;
+	while ((9 <= *nptr && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			is_nega = 1;
+		nptr++;
+	}
+	while ('0' <= *nptr && *nptr <= '9')
+	{
+		result *= 10;
+		result += (*nptr) - '0';
+		nptr++;
+	}
+	if (is_nega)
+		result *= -1;
+	return (result);
+}
+
+size_t	get_current_time(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+	{
+		printf("Error getting time of day\n");
+		return (0);
+	}
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
