bef5201de42f1f21db465858b2a7b278c5f9b2c9
jianwong <jianwong@student.42kl.edu.my>
2025-01-09 16:54:38 +0800

    more proto

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -6,11 +6,45 @@
 /*   By: jianwong <jianwong@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 23:52:53 by jianwong          #+#    #+#             */
-/*   Updated: 2025/01/09 00:18:27 by jianwong         ###   ########.fr       */
+/*   Updated: 2025/01/09 16:54:17 by jianwong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo_bonus.h"
+
+int	create_child(int *vars)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	while (i < vars[NUM_PHILOS])
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			printf("Fork has failed\n");
+			return (1);
+		}
+		if (pid == 0)
+			philo_routine();
+		else
+			i++;
+	}
+	return (0);
+}
+
+int	kill_child()
+{
+	pid_t	w;
+
+	while (1)
+	{
+		w = wait(NULL);
+		if (w == -1)
+			break ;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,6 +56,9 @@ int	main(int argc, char **argv)
 	metadatas = create_philo_metadatas(vars, &forks);
 	if (!metadatas)
 		return (2);
-
+	if (create_child())
+		return (3);
+	if (kill_child())
+		return (4);
 	return (0);
 }
